b0b17abca2c1a2128a4daa2111c982c042591d55
dharmik803 <dgajjar974@gmail.com>
2023-10-13 14:26:51 +0530

    App.userouting added

--- Contact-management-devextreme/Program.cs
+++ Contact-management-devextreme/Program.cs
@@ -32,8 +32,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
-app.UseAuthorization();
 app.UseRouting();
+app.UseAuthorization();
 app.MapControllers();
 app.UseEndpoints(endpoints =>
 {
